337c3030135423291a1daded566e3df530c79cec
tsteelematc <tsteele@madisoncollege.edu>
2024-10-09 08:33:38 -0500

    Experimenting for first Madcol IT Front End Dev tech-talk : - )

---
+++ main.py
@@ -0,0 +1,42 @@
+from fasthtml.common import *
+import random
+
+app, rt = fast_app()
+
+
+@rt("/")
+def get():
+    color_list = [
+        "silver",
+        "pink",
+        # 1,
+        # 2,
+    ]
+
+    return Div(
+        Div(
+            P("Hello World!"),
+        ),
+        hx_get="/change",
+        style=f"background-color: {random.choice(color_list)}; padding: 20px",
+        # cls="foo",
+        _class="bar",
+    )
+
+
+@rt("/change")
+def get():
+    return Div(
+        H1("Foo"),
+        H3("Bar"),
+        H5("Cat"),
+        # Card(
+        #     "Foo",
+        #     header="Header",
+        #     footer="Footer",
+        # ),
+        style="background-color: pink;",
+    )
+
+
+serve()
